9f7f44c375ef59d9c39d43b4beef1b3a578deb6b
NgChunMan <122211045+NgChunMan@users.noreply.github.com>
2024-12-15 14:30:48 +0800

    Update test_gradient_descent.py

--- tests/test_gradient_descent.py
+++ tests/test_gradient_descent.py
@@ -1,8 +1,11 @@
+import os
 from src.models.gradient_descent import gradient_descent_multi_variable
 from src.data_utils import load_data
 
 def test_gradient_descent_multi_variable():
-    data_filepath = './data/housing_data.csv'
+    project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    data_filepath = os.path.join(project_root, 'data', 'housing_data.csv')
+    
     X, y = load_data(data_filepath)
 
     _, _, loss = gradient_descent_multi_variable(X, y, lr = 1e-5, number_of_epochs = 250)
